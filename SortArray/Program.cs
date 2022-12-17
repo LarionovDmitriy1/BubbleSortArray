3e19e334b6d3c6fb0c44f1070ddb7224f772e2e6
Студент1 <1@DESKTOP-7RO4MU5>
2022-12-17 10:49:38 +0300

    Добавьте файлы проекта.

---
+++ SortArray/Program.cs
@@ -0,0 +1,23 @@
+﻿using Sort_Array;
+
+Console.WriteLine("Введите числа для сортировки");
+Console.WriteLine();
+string[] words = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+int[] array = new int[words.Length];
+for (int i = 0; i < words.Length; i++)
+{
+    array[i] = int.Parse(words[i]);
+}
+
+var result = Sort.SortArray(array);
+if (result.Length == 0 || result is null)
+{
+    Console.WriteLine("Массив пустой");
+    return;
+}
+
+Console.Write($"Отсортированный массив - ");
+foreach (int res in result)
+{
+    Console.Write($"{res} ");
+}
---
+++ SortArray/Sort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort_Array;
+
+public class Sort
+{
+    public static int[] SortArray(int[] array)
+    {
+        if (array == null || array.Length == 0)
+        {
+            return new int[] { };
+        }
+        for (int i = 0; i < array.Length; i++)
+        {
+            for (int j = 0; j < array.Length - 1 - i; j++)
+            {
+                if (array[j] > array[j + 1])
+                {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        return array;
+    }
+}
